bd8eeee211ae6dba87099ea37d22930032e20248
Mar Cabrera <mar.cabrera@mongodb.com>
2023-01-25 13:25:25 +0000

    Add Apple Sign In button and authentication flow

--- apple-sign-in/apple-sign-in/ViewController.swift
+++ apple-sign-in/apple-sign-in/ViewController.swift
@@ -6,14 +6,55 @@
 //
 
 import UIKit
+import AuthenticationServices
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var appleSignInButton: ASAuthorizationAppleIDButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        setupAppleSignInButton()
     }
 
+    private func setupAppleSignInButton() {
+        appleSignInButton.addTarget(self, action: #selector(handleAppleIdRequest), for: .touchUpInside)
+        appleSignInButton.cornerRadius = 10
+    }
 
+    @objc func handleAppleIdRequest() {
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.performRequests()
+    }
+}
+
+extension ViewController: ASAuthorizationControllerDelegate {
+    // Delegate that handles successful Sign In
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+            let userIdentifier = appleIDCredential.user
+            let fullName = appleIDCredential.fullName
+            let email = appleIDCredential.email
+
+            print(userIdentifier)
+            print(fullName)
+            print(email)
+            // Note: If the user decides to hide their email on authentication, full name will return an empty string and email will be nil
+            
+            // Retrieve AppleID token so I can send it to Realm and authenticate myself
+        }
+    }
+
+    // Delegate function that handles authentication error on Apple Sign In
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        print("Oops! There was an error")
+        // Display alert for error
+    }
 }
 
